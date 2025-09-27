8876ff4c662a374dbc5d90ac95cc5043825c4c47
kadekedwin <kadekedwin12@gmail.com>
2025-09-27 21:57:47 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import Dashboard from '@/components/Dashboard'
 import { initDatabase } from '@/lib/db'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   await initDatabase()
 
